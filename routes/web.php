10888c498c8cb4fc5cf37a8c32d4dc63a56528b7
liujun <995672751@qq.com>
2019-01-13 12:51:19 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-Route::get('/', 'PagesController@root')->name('root');
-
-
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 // 在之前的路由里加上一个 verify 参数
 Auth::routes(['verify' => true]);
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
@@ -14,4 +13,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 
+    Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+    Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+
 });
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
